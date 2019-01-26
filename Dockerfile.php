f63f193f26136da1db7157bf03383b117f28edde
Ben Jackson <b@benjackson.email>
2019-01-26 12:27:23 +1100

    add ldap extension to dockerfile.php

--- Dockerfile.php
+++ Dockerfile.php
@@ -3,4 +3,13 @@ FROM ${CLI_IMAGE} as cli
 
 FROM amazeeio/php:7.2-fpm
 
+## Add the dev package for ldap (openldap-dev)
+## Install the extension ldap and configure it
+RUN apk update \
+    && apk add --no-cache openldap-dev \
+    && docker-php-source extract \
+    && docker-php-ext-install ldap \
+    && docker-php-ext-configure ldap \
+    && docker-php-source delete
+
 COPY --from=cli /app /app
